6558b4f82eecb23656a0f12d71cd036ef91f0a7d
Nghia Tran <thnghia6063@gmail.com>
2021-11-26 13:37:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+nombre= 6
+a= 2*nombre
+b= a-5
+c= 3*b
+d= c+15
+e= d/6
+print(e)
+
---
+++ try.py
@@ -0,0 +1,7 @@
+a=eval(input('Saisir a: '))
+b=a**2-5
+print('The value of a*2-5 is:',b)
+
+c=eval(input('Saisir b: '))
+d=(3*c+18)/3
+print('The value of (3*c+18)/3 is:',d)
